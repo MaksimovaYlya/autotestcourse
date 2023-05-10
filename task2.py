c08baf4df1e6cebe6e2e945d11a9d7371dc185c1
MaksimovaYlya <132578363+MaksimovaYlya@users.noreply.github.com>
2023-05-10 10:35:13 +0300

    Add files via upload

--- task2.py
+++ task2.py
@@ -8,7 +8,6 @@ def get_list_info(lst):
     max_elem = max(lst)
     sum_list = sum(lst)
     average = round(sum_list / len(lst), 2)
-
     return min_elem, max_elem, sum_list, average
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
